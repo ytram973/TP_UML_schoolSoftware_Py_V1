23247155ec8cfff0011742bb1fcae9dd76ae4b22
DavidB <david.bernard@fms-ea.com>
2025-11-21 10:19:56 +0100

    teacher.py modified to fit address object

--- teacher.py
+++ teacher.py
@@ -1,27 +1,26 @@
 
 from person import Person
+from address import Address
 
 
 class Teacher(Person):
 
     date: str
 
-    def __init__(self, firstName: str, lastName: str, age: int, date: str, road: str, city: str, zipCode: int):
+    def __init__(self, firstName: str, lastName: str, age: int, date: str, address: Address):
         """
          Initialize a Teacher object.
         """
         super().__init__(firstName, lastName, age)
         self.date = date
-        self.set_address(road, city, zipCode)
+        self.address = address
 
 
     def set_address(self, road: str, city: str, zipCode: int):
         """
          Set the address of the teacher.
         """
-        self.road = road
-        self.city = city
-        self.zipCode = zipCode
+        self.address = Address(road, city, zipCode)
 
 
     def get_date(self):
