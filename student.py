dd46845179caf3a31f66cd57d6b0aeb27b29aa5b
DavidB <david.bernard@fms-ea.com>
2025-11-21 09:55:42 +0100

    docstrings added to student.py and teacher.Py

--- student.py
+++ student.py
@@ -5,33 +5,58 @@ class Student(Person):
     number: int
 
     def __init__(self, number: int, firstName: str, lastName: str, age: int, road: str, city: str, zipCode: int):
+        """
+         Initialize a Student object.
+        """
         super().__init__(firstName, lastName, age)
         self.number = number
         self.set_address(road, city, zipCode)
         self.lessons = []
 
+
     def set_address(self, road: str, city: str, zipCode: int):
+        """
+         Set the address of the student."""
         self.road = road
         self.city = city
         self.zipCode = zipCode
 
+
     def get_number(self):
+        """
+         Get the student number.
+         """
         return self.number
     
+
     def set_number(self, number: int):
+        """
+         Set the student number.
+        """
         self.number = number
 
+
     def add_lesson(self, lesson: str):
+        """
+         Add a lesson to the student's list of lessons.
+         """
         if lesson is None:
             raise ValueError("Lesson cannot be None")
         
         if lesson not in self.lessons:
             self.lessons.append(lesson)
         
+
     def get_lessons(self):
+        """
+        Get the list of lessons for the student.
+        """
         return self.lessons
 
+
     def __str__(self):
+        """
+         Return a string representation of the student."""
         return (f"Student{{"
             f"number : {self.number}, "
             f"name : {self.getFullName()}', "
--- teacher.py
+++ teacher.py
@@ -7,20 +7,34 @@ class Teacher(Person):
     date: str
 
     def __init__(self, firstName: str, lastName: str, age: int, date: str, road: str, city: str, zipCode: int):
+        """
+         Initialize a Teacher object.
+        """
         super().__init__(firstName, lastName, age)
         self.date = date
         self.set_address(road, city, zipCode)
 
 
     def set_address(self, road: str, city: str, zipCode: int):
+        """
+         Set the address of the teacher.
+        """
         self.road = road
         self.city = city
         self.zipCode = zipCode
 
+
     def get_date(self):
+        """
+         Get the hire date of the teacher.
+        """
         return self.date
     
+
     def __str__(self):
+        """
+         Return a string representation of the teacher.
+        """
         return (f"Teacher{{"
             f"name : {self.getFullName()}', "
             f"age : {self.getAge()}, "
