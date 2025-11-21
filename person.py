8a75a23f6eef87ff2ec258678702c0d657593236
Amandine Grard-Laurent <amandine.grard-laurent@fms-ea.com>
2025-11-21 09:30:45 +0100

    pull main

--- person.py
+++ person.py
@@ -1,13 +1,14 @@
-from typing import ClassVar
 
+from address import Address
 
 class Person:
-    #persons = []
 
-    def __init__(self, firstname: str, lastname: str, age: int):
+    def __init__(self, firstname: str, lastname: str, age: int, address: Address):
         self.firstname = firstname
         self.lastname = lastname
         self.age = age
+        self.address = address
+
         #Person.persons.append(self)
 
     def get_firstname(self) -> str:
@@ -22,6 +23,20 @@ class Person:
     def get_fullname(self) -> str:
         return self.firstname + " " + self.lastname
 
+    def get_road(self):
+        return self.address.road
+
+    def get_city(self):
+        return self.address.city
+
+    def get_zip_code(self):
+        return self.address.zip_code
+
     def __repr__(self) -> str:
-        return f"{self.firstname}, {self.lastname}, {self.age}ans"
+        return (f"{self.firstname}, "
+                f"{self.lastname}, "
+                f"{self.age}ans : "
+                f"{self.address.road} "
+                f"{self.address.zip_code} "
+                f"{self.address.city}")
 
